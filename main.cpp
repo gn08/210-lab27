c70c975c1234ac9c61f5a4d2b10b379257b1726c
gn08 <gabbyfilipova@gmail.com>
2024-10-30 10:15:50 -0700

    commit 10:15

--- main.cpp
+++ main.cpp
@@ -12,6 +12,36 @@ int main() {
         cout << "Menu: \n" << "1. Add Villager\n" << "2. Delete Villager\n" << "3. Increase Friendship\n"
             << "4. Decrease Friendship\n" << "5. Search for Villager\n" << "6. Exit\n";
         cin >> choice;
+
+        if (choice == 1){
+            string name;
+            string species;
+            string catchphrase;
+            int friendship;
+            cout << "Villager Name: ";
+            cin >> name;
+            cout << "Friendship level: ";
+            cin >> friendship;
+            cout << "Species: ";
+            cin >> species;
+            cout << "Catchphrase: ";
+            cin.ignore();
+            getline(cin, catchphrase);
+            villagers[name] = make_tuple(friendship, species, catchphrase);
+            cout << name << " added. \n";
+        } else if (choice == 2){
+
+        } else if (choice == 3 || choice == 4){
+
+        } else if (choice == 5){
+
+        } else if (choice == 6){
+
+        } else {
+
+        } while ();
+
+        return 0;
     }
 
     // insert elements into the map
